036ee8b314dd64756eacc447ebece225ea4d9008
andytang9 <90296770+andytang9@users.noreply.github.com>
2021-11-09 19:04:40 +0700

    .

--- server/routes/router.js
+++ server/routes/router.js
@@ -26,6 +26,6 @@ route.get('/update-user', services.update_user)
 route.post('/api/users', controller.create);
 route.get('/api/users', controller.find);
 route.put('/api/users/:id', controller.update);
-route.get('/api/users/:id', controller.delete);
+route.delete('/api/users/:id', controller.delete);
 
 module.exports = route
